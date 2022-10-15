b084355939ef8bb74c0bcf9ee3ff05a24b93c2e6
Ivan <nazgoool1@gmail.com>
2022-10-15 17:01:07 +0500

    complite task 3

--- HomeWork/HWSeminar_1/task_3/Program.cs
+++ HomeWork/HWSeminar_1/task_3/Program.cs
@@ -4,3 +4,17 @@
 -3 -> нет
 7 -> нет
 */
+int number = Convert.ToInt32(Console.ReadLine());
+    if (number % 2 == 0)
+        {
+            Console.Write(number);
+            Console.Write(" -> ");
+            Console.Write("yes  ");
+        }
+    else
+        {
+            Console.Write(number);
+            Console.Write(" -> ");
+            Console.Write("no  ");
+        }    
+
