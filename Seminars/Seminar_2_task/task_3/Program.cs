9fc92a0d0a765cbb18a436dbb882c84147fa6b1d
Ivan <nazgoool1@gmail.com>
2022-10-16 12:50:05 +0500

    complit task 3_1

--- Seminars/Seminar_2_task/task_3/Program.cs
+++ Seminars/Seminar_2_task/task_3/Program.cs
@@ -3,7 +3,7 @@
 
 void Multiplicty_check (int number_new)
 {
-    if ((number_new % 7 == 0) && (number_new % 23 == 0))
+    if ((number_new % 7 == 0) & (number_new % 23 == 0))
         {
             Console.WriteLine($"{number_new}-->yes");
         }
