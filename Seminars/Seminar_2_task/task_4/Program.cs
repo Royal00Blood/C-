2e1ad0c6b75e367717f3938188bf87a63b087932
Ivan <nazgoool1@gmail.com>
2022-10-16 13:07:34 +0500

    correct sintaksis

--- Seminars/Seminar_2_task/task_4/Program.cs
+++ Seminars/Seminar_2_task/task_4/Program.cs
@@ -3,7 +3,7 @@ int number_2 = Convert.ToInt32(Console.ReadLine());
 
 void Multiplicty_check (int number_new_1,int number_new_2)
 {
-    if (number_new_1*number_new_1 == number_new_2 || number_new_1 == number_new_2*number_new_2)
+    if (number_new_1 * number_new_1 == number_new_2 || number_new_1 == number_new_2 * number_new_2)
         {
             Console.WriteLine($"{number_new_1},{number_new_2} -- > yes");
         }
