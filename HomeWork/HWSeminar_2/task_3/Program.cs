f0809a51da7ffbadd1e628a880ab2a82982063e3
Ivan <nazgoool1@gmail.com>
2022-10-16 14:44:17 +0500

    complit task 3

--- HomeWork/HWSeminar_2/task_3/Program.cs
+++ HomeWork/HWSeminar_2/task_3/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string Check_day(int day)
+{
+    if(day <= 7 && day > 0)
+    {
+        if (day < 6)
+            {
+                return "No, it is working day";
+            } 
+        else
+            {
+                return "Yes, it is holiday";
+            }
+    }
+    else
+        {
+            return "It's not day of the week";
+        }
+}
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{number}-->{Check_day(number)}");
