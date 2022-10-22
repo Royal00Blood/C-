50bcd4e1cc72f2120ba35f5f4b6d81a9234ea8b6
Ivan <nazgoool1@gmail.com>
2022-10-22 12:58:48 +0400

    complite seminat tasks

--- Seminars/Seminar_3/task_2/Program.cs
+++ Seminars/Seminar_3/task_2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool is_parsed_x_1 = int.TryParse(Console.ReadLine(),out int x_1);
+bool is_parsed_y_1 = int.TryParse(Console.ReadLine(),out int y_1);
+bool is_parsed_x_2 = int.TryParse(Console.ReadLine(),out int x_2);
+bool is_parsed_y_2= int.TryParse(Console.ReadLine(),out int y_2);
+
+if (!is_parsed_x_1 || !is_parsed_x_2 || !is_parsed_y_1 || !is_parsed_y_2)
+    {
+        Console.WriteLine("Error, type is not compilit");
+        return;
+    }
+
+double Distance(int Ax,int Ay, int Bx, int By)
+    {
+        double distance = Math.Sqrt(Math.Pow((Ax - Bx),2) + Math.Pow((Ay-By),2));
+        return distance;
+    }
+
+Console.WriteLine($"value distance = {Distance(x_1,y_1,x_2,y_2)}");
--- Seminars/Seminar_3/task_3/Program.cs
+++ Seminars/Seminar_3/task_3/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool is_parsed_number = int.TryParse(Console.ReadLine(),out int number);
+
+if (!is_parsed_number)
+    {
+        Console.WriteLine("Error, type is not compilit");
+        return;
+    }
+
+
+void Work (int number_2)
+{
+    int[] array = new int[number_2+1];
+    Console.Write($"{number_2}-->");
+    for(int i=1; i <= number_2; i++)
+        {
+            array[i] = i * i;   
+            Console.Write($" {array[i]} ");
+        }
+}
+
+Work(number);
