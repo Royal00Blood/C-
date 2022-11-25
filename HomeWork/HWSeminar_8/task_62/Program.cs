3279a41024e382c59ed1208f7af9d946fcea2c56
Ivan <nazgoool1@gmail.com>
2022-11-25 18:28:03 +0400

    chnge task 62

--- HomeWork/HWSeminar_8/task_62/Program.cs
+++ HomeWork/HWSeminar_8/task_62/Program.cs
@@ -7,3 +7,26 @@
 11 16 15 06
 10 09 08 07
 */
+void Print2DarraySpiral(int [,] array)
+{
+    int Ni = 4;
+    int Nj = 4;
+    int i1 = 0;
+    int j1 = 0;
+    int number = 0;
+    for (int i = i1; i < Ni; i++)
+    {
+        for (int j = j1 ; j < Nj; j++)
+        {
+            array[j,i] = number + 1;
+        }
+        j1++;
+    }
+    for (int i = 0; i < Ni; i++)
+    {
+        for (int j = 0; j < Nj; j++)
+        {
+            array[j,i] = number + 1;
+        }
+    }
+}
