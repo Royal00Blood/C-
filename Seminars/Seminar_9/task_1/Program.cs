64f0f4ed713a99dafa4b3e8e0a79d62ce0c05430
Ivan <nazgoool1@gmail.com>
2022-11-26 11:37:28 +0400

    task 1,2 complite

--- Seminars/Seminar_9/task_1/Program.cs
+++ Seminars/Seminar_9/task_1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Input n--> ");
+bool isParsed_2 = int.TryParse(Console.ReadLine(),out int N);
+Console.WriteLine($"Count number{N}:");
+if( !isParsed_2)
+{
+    Console.WriteLine("Error");
+    return;
+}
+
+int number = 1;
+PrintNextNumber(number, N);
+
+void PrintNextNumber(int m,int n)
+{
+Console.Write($"{number} ");
+    if(number < n)
+    {
+        number++;
+        PrintNextNumber(number,n);
+    }
+}
--- Seminars/Seminar_9/task_2/Program.cs
+++ Seminars/Seminar_9/task_2/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Input start number--> ");
+bool isParsed_1 = int.TryParse(Console.ReadLine(),out int M);
+Console.Write("Input end number--> ");
+bool isParsed_2 = int.TryParse(Console.ReadLine(),out int N);
+Console.WriteLine($"Numbers {M} --> {N}:");
+if(!isParsed_1 || !isParsed_2)
+{
+    Console.WriteLine("Error");
+    return;
+}
+
+if (M<N)
+{
+    PrintNextNumber(M, N);
+}
+else
+{
+    Console.WriteLine("Error start number > end number");
+}
+
+void PrintNextNumber(int m,int n)
+{
+ Console.Write($"{m}");
+ if(m < n)
+ {
+    m++;
+    PrintNextNumber(m,n);
+ }
+}
